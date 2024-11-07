32d19fb30a34da9f1da7f854266762a7ec02d62c
Mackenzie Anderson <mackenzie@Chuls-MacBook-Pro.local>
2024-11-07 08:19:31 -0800

    created const for quiz questions json

---
+++ server/quiz_modules/QuizQuestionsModule.js
@@ -0,0 +1,41 @@
+const quizQuestions = {
+    "Q1": {
+        "Question": "How would you categorize your plant?",
+        "Choices": {
+            "A": "Flowering",
+            "B": "Succulent",
+            "C": "Fern",
+            "D": "Tropical Tree/Shrub",
+            "E": "Vine/Climber"
+        }
+    },
+    "Q2": {
+        "Question": "Which direction does the window nearest to your plant face?",
+        "Choices": {
+            "A": "North",
+            "B": "South",
+            "C": "East",
+            "D": "West",
+            "E": "My plant is not near a window"
+        }
+    },
+    "Q3": {
+        "Question": "Approximately how many hours of daylight are there where you live?",
+        "Choices": {
+            "A": "Less than 6 hrs",
+            "B": "6 - 10 hrs",
+            "C": "11 - 15 hrs",
+            "D": "16 - 20 hrs",
+            "E": "More than 20 hrs"
+        }
+    },
+    "Q4": {
+        "Question": "Do you use a UV lamp near your plant?",
+        "Choices": {
+            "A": "Yes",
+            "B": "No"
+        }
+    }
+};
+
+export default quizQuestions;
