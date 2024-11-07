10e3e22c498646c336e41d47d3fa487c044aba9b
Mackenzie Anderson <mackenzie@Chuls-MacBook-Pro.local>
2024-11-06 21:15:40 -0800

    create basic get route to test functionality

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to microservice A'});
+})
+
 app.listen(process.env.PORT, () => {
     console.log('Listening on port', process.env.PORT);
 })
